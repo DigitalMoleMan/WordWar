eb0483628d7bfbd7342ef72ca59219e017bae8a0
digitalmoleman <digitalmoleguy@gmail.com>
2019-12-10 13:40:21 +0100

    basic movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+    // Start is called before the first frame update
+
+    public float moveSpeed = 1;
+
+    
+    void Start()
+    {
+        
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform t = GetComponent<Transform>();
+        Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(GetInputDirection() * moveSpeed);
+
+
+       if (Input.GetButton("Fire1")) {
+            Debug.Log("Fire!");
+
+        }
+    }
+
+    Vector3 GetInputDirection() {
+        Vector3 inputDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        return inputDir;
+    }
+}
